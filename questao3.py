113bb6b56cef4e9f15865924cf82bc15acd16a58
Luan Santos <luanssantos1228@gmail.com>
2026-03-15 18:10:41 -0300

    Questão 3 criada

--- questao3.py
+++ questao3.py
@@ -4,12 +4,10 @@ limite_inferior = int(input('Limite inferior: '))
 limite_superior = int(input('Limite superior: '))
 tamanho = int(input('Informe o tamanho da lista: '))
 lista = [randint(limite_inferior, limite_superior) for _ in range(tamanho)]
+maior = lista[0]
 
-soma = 0
-media = 0
+for i in range(1, tamanho):
+    if lista[i] > maior:
+        maior = lista[i]
 
-for c in lista:
-    soma += c
-    media = soma / tamanho
-
-print(f'A média de todos os elementos da lista é {media:.2f}')
+print(f'O maior valor da lista {lista} é {maior}.')
