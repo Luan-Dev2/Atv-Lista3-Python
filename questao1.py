44c189154592bdef1ec6a573851c8720eb8345a3
Luan Santos <luanssantos1228@gmail.com>
2026-03-14 23:10:02 -0300

    Questão 1 criada!

--- questao1.py
+++ questao1.py
@@ -1,1 +1,12 @@
-limite_inferior = int(input('Digite o limite inferior da lista: '))
+from random import randint
+
+tamanho = int(input('Informe o tamanho da lista: '))
+lista = []
+soma = 0
+
+for c in range(tamanho):
+    lista.append(randint(0, 10))
+for c in lista:
+    soma += c
+
+print(f'A soma entre todos os elementos da lista {lista} é {soma}.')
