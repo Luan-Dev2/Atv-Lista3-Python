bb0a6724bb88bb8c22e668b7ed4b4629cd9df540
Luan Santos <luanssantos1228@gmail.com>
2026-03-15 17:58:10 -0300

    Alterações questão 1 e criação questão 2!

--- questao2.py
+++ questao2.py
@@ -3,12 +3,14 @@ from random import randint
 limite_inferior = int(input('Limite inferior: '))
 limite_superior = int(input('Limite superior: '))
 tamanho = int(input('Informe o tamanho da lista: '))
-lista = []
-soma = 0
+lista = [randint(limite_inferior, limite_superior) for _ in range(tamanho)]
 
-for c in range(tamanho):
-    lista.append(randint(limite_inferior, limite_superior))
+soma = 0
+media = 0
+
 for c in lista:
     soma += c
+    media = soma / tamanho
 
-print(f'A soma entre todos os elementos da lista {lista} é {soma}.')
+print(lista)
+print(f'A média de todos os elementos da lista é {media:.2f}')
